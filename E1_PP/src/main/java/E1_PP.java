e80d3ad35d5e66ef7b31ad825897a758dd46a9e6
gugeldot <gugeldot23@gmail.com>
2024-11-20 11:24:49 +0100

    Doc push (1)

--- E1_PP/src/main/java/E1_PP.java
+++ E1_PP/src/main/java/E1_PP.java
@@ -1,11 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
-/**
- *
- * @author Gugeldot
- */
 public class E1_PP {
 
     public static void main(String[] args) {
