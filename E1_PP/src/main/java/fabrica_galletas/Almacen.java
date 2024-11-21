0bf9b4ad771ea8f565c5148b17b2e88d9b5e69bc
gugeldot <gugeldot23@gmail.com>
2024-11-21 20:01:09 +0100

    implementacion basica por locks

--- E1_PP/src/main/java/fabrica_galletas/Almacen.java
+++ E1_PP/src/main/java/fabrica_galletas/Almacen.java
@@ -1,26 +1,73 @@
 package fabrica_galletas;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+import misc.Logger;
+
 /*
-*   Objetivo: 
+*   Objetivo: Implementacion del almace
     Notas: 
-        + 
+        + Aproximación mediante locks 
+        + He usado locks y no semaforos porque veia poco intuitivo de entender
+            e interorizar.
     USO GENERAL: 
 *   Autor: Rodrigo Palomo 
  */
 
+
 public class Almacen {
+
+    private Logger logger;
     private int CAPACIDAD_MAXIMA;
     private int capacidad_actual = 0; //READONLY por el resto
 
-    public Almacen(int CAPACIDAD_MAXIMA, int capacidad_actual) {
+    private final ReentrantLock lock = new ReentrantLock();
+    private final Condition noLleno = lock.newCondition();
+    private final Condition noVacio = lock.newCondition();
+
+    public Almacen(int CAPACIDAD_MAXIMA,  Logger logger) {
         this.CAPACIDAD_MAXIMA = CAPACIDAD_MAXIMA;
-    }
-    
-    public int getCapacidad_actual() {
-        return capacidad_actual;
+        this.logger = logger;
     }
 
-    public void almacenar(int cantidad) {}
-    public void comer() {}
-    
+    public synchronized int getCapacidad_actual() { 
+       //Usado synchronized porque se lee mejor que lock con try y finally
+       return capacidad_actual;
+    }
+
+    public void almacenar(int cantidad) {
+        lock.lock();
+        try {
+            while (capacidad_actual + cantidad > CAPACIDAD_MAXIMA) {
+                System.out.println("Almacén lleno. Esperando a que se libere espacio...");
+                noLleno.await();
+            }
+            // Fin de la espera, "Ha entrado"
+            capacidad_actual += cantidad;
+            System.out.println("Se almacenaron " + cantidad + " galletas. Capacidad actual: " + capacidad_actual);
+            noVacio.signalAll(); 
+        }
+        catch (Exception e) { System.out.println(e); }
+        finally { lock.unlock(); }
+        
+
+    }
+
+    public void comer() {
+        lock.lock();
+        try {
+            while (capacidad_actual < 100) {
+                System.out.println("No hay suficientes galletas para consumir. Esperando...");
+                noVacio.await();
+            }
+            
+            // Fin de la espera, "Ha entrado"
+            capacidad_actual -= 100;
+            System.out.println("Se consumieron 100 galletas. Capacidad actual: " + capacidad_actual);
+            noLleno.signalAll(); 
+        } 
+        catch (Exception e) { System.out.println(e); }
+        finally { lock.unlock(); }
+    }
+
 }
