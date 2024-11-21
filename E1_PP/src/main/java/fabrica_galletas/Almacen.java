f1dfcf3a44fe591a9ace2b0dde3ef14325533866
gugeldot <gugeldot23@gmail.com>
2024-11-21 20:40:52 +0100

    Documentacion + tweaks

--- E1_PP/src/main/java/fabrica_galletas/Almacen.java
+++ E1_PP/src/main/java/fabrica_galletas/Almacen.java
@@ -14,32 +14,53 @@ import misc.Utilidades;
     USO GENERAL: 
 *   Autor: Rodrigo Palomo 
  */
+
 public class Almacen {
 
+    // Atributos
     private Logger logger;
     private int CAPACIDAD_MAXIMA;
     private int capacidad_actual = 0; //READONLY por el resto
     private String ID; //READONLY
 
+    
+    // Locks & Conditions
     private final ReentrantLock lock = new ReentrantLock();
     private final Condition noLleno = lock.newCondition();
     private final Condition noVacio = lock.newCondition();
 
+    
     public Almacen(String ID, int CAPACIDAD_MAXIMA, Logger logger) {
         this.CAPACIDAD_MAXIMA = CAPACIDAD_MAXIMA;
         this.ID = ID;
         this.logger = logger;
     }
 
+     /*
+            OBJ: getter capacidad total
+            PRE: -
+            POST: -  
+            EXTRA: Usado synchronized porque se lee mejor que lock con try y finally
+    */
     public synchronized int getCapacidad_actual() {
-        //Usado synchronized porque se lee mejor que lock con try y finally
         return capacidad_actual;
     }
-
+    
+    /*
+            OBJ: getter ID
+            PRE: -
+            POST: -  
+    */
     public String getID() {
         return ID;
     }
 
+    /*
+            OBJ: Almacena en capacidad actual en numero de galletas indicado
+                 Usa exclusion mutua mendiante locks y conditions
+            PRE: -
+            POST: Capacidad actual actualizada o hilo en espera para ello  
+    */
     public void almacenar(int cantidad, String autor) {
         lock.lock();
         try {
@@ -53,22 +74,27 @@ public class Almacen {
             Thread.sleep(Utilidades.numeroRandom(2, 4));
 
             logger.add(ID,autor, "Se almacenaron " + cantidad + " galletas. Capacidad actual: " + capacidad_actual);
+            
             noVacio.signalAll();
-        } catch (Exception e) {
-            System.out.println(e);
-        } finally {
-            lock.unlock();
-        }
+        } 
+        catch (Exception e) { System.out.println(e); } 
+        finally { lock.unlock(); }
 
     }
-
+    
+    /*
+            OBJ: Consume galletas del almacen
+                 Usa exclusion mutua mendiante locks y conditions
+            PRE: Pensada para interaccion por boton en interfaz "Comer"
+            POST: Capacidad actual actualizada o hilo en espera para ello 
+    */
     public void comer() {
         String autor = "Usuario";
         lock.lock();
         try {
             while (capacidad_actual < 100) {
                 logger.add(ID,autor, "No hay suficientes galletas para consumir. Esperando...");
-                noVacio.await();
+                noVacio.await(); // En un futuro es probable que no haya que dejar el hilo congelado, si no descartar la solicitud
             }
 
             // Fin de la espera, "Ha entrado"
@@ -76,11 +102,10 @@ public class Almacen {
 
             logger.add(ID,autor, "Se consumieron 100 galletas. Capacidad actual: " + capacidad_actual);
             noLleno.signalAll();
-        } catch (Exception e) {
-            System.out.println(e);
-        } finally {
-            lock.unlock();
+            
         }
+        catch (Exception e) { System.out.println(e); } 
+        finally { lock.unlock(); }
     }
 
 }
--- E1_PP/src/main/java/mainRun/Main.java
+++ E1_PP/src/main/java/mainRun/Main.java
@@ -7,7 +7,6 @@ public class Main {
     public static void main(String[] args) {
         // Instancia inicial objeto logger, pasar por parametro a toda clase instanciada
         // Nota: Se ha puesto directorio anterior para que se muestre fuera del proyecto
-        
         Logger logger = new Logger("../evolucionGalletas.txt",false); 
         
         Almacen almacen = new Almacen("ALMACEN1",1000,logger);
