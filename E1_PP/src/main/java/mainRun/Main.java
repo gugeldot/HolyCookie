f8779cc0bce101b8381903851b562327db1b21a8
gugeldot <gugeldot23@gmail.com>
2024-11-21 20:04:18 +0100

    instancia en main

--- E1_PP/src/main/java/mainRun/Main.java
+++ E1_PP/src/main/java/mainRun/Main.java
@@ -1,13 +1,16 @@
 package mainRun;
 
 import misc.Logger;
+import fabrica_galletas.*;
 
 public class Main {
     public static void main(String[] args) {
         // Instancia inicial objeto logger, pasar por parametro a toda clase instanciada
         // Nota: Se ha puesto directorio anterior para que se muestre fuera del proyecto
+        
         Logger logger = new Logger("../evolucionGalletas.txt",false); 
         
+        Almacen almacen = new Almacen(1000,logger);
         
         
         
