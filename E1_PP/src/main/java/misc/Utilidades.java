2cae873a4fc8e6f16e22fdfece4ff5c3ee4c488e
gugeldot <gugeldot23@gmail.com>
2024-11-21 20:18:18 +0100

    Creacion de Utilidades
    para metodos sin contexto especifico

---
+++ E1_PP/src/main/java/misc/Utilidades.java
@@ -0,0 +1,27 @@
+package misc;
+
+/*
+*   Objetivo: Metodos para funciones sin un contexto especifico y potencialmente
+                muy usadas.
+    Notas: 
+        + 
+*   Autor: Rodrigo Palomo 
+ */
+
+import java.util.Random;
+
+
+public class Utilidades {
+    
+    public static int numeroRandom (int minimo,int maximo){
+        /*
+            OBJ: Devuelve numero entero comprendido entre minimo y maximo
+                    (ambos inclusive)
+            PRE: maximo >= minimo
+            POST: -  
+         */
+        Random random = new Random();
+        return random.nextInt((maximo - minimo) + 1) + minimo;
+    }
+    
+}
