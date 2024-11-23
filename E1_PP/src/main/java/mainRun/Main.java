5abb691077a5203d16142f31e63328901f5d2054
gugeldot <gugeldot23@gmail.com>
2024-11-23 02:07:03 +0100

    Borrada la clase adminHorno
    distribuida la funcionalidad de la clase en la relacion Repostero - horno sin intermediario (una puta mierda de implement pero bueno es lo q pide el enunciado yo que se macho)

--- E1_PP/src/main/java/mainRun/Main.java
+++ E1_PP/src/main/java/mainRun/Main.java
@@ -3,34 +3,49 @@ package mainRun;
 import misc.Logger;
 import fabrica_galletas.*;
 import java.util.logging.Level;
+import misc.Utilidades;
 
 public class Main {
+    // PRUEBAS 
+    public static void proc_Simulador_repostero(Horno[] hornos){
+         // Crear y lanzar un hilo usando una expresión lambda
+        Thread hilo = new Thread(() -> {
+            while(true){
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException ex) {
+                    java.util.logging.Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                hornos[Utilidades.numeroRandom(0, 2)].retirarGalletas(200);
+            }
+        });
 
+        // Iniciar el hilo
+        hilo.start();
+    }
     public static void main(String[] args) throws InterruptedException {
         final int MAX_GALLETAS_HORNO = 200;
         final int NUMERO_REPOSTEROS = 5;
-
-        // Instancia inicial objeto logger, pasar por parametro a toda clase instanciada
-        // Nota: Se ha puesto directorio anterior para que se muestre fuera del proyecto
+        
         Logger logger = new Logger("../evolucionGalletas.txt", true);
         Almacen almacen = new Almacen("ALMACEN1", 1000, logger);
-        
-        Horno[] arrayHornos = {new Horno("Horno1", MAX_GALLETAS_HORNO, logger),
-            new Horno("Horno2", MAX_GALLETAS_HORNO,logger),
-            new Horno("Horno3", MAX_GALLETAS_HORNO,logger)};
-        
-        AdministradorHornos adminHornos = new AdministradorHornos(arrayHornos, logger);
 
-        adminHornos.arrancarHornos();
+        Horno[] hornos = {  new Horno("Horno1", MAX_GALLETAS_HORNO, logger),
+                            new Horno("Horno2", MAX_GALLETAS_HORNO, logger),
+                            new Horno("Horno3", MAX_GALLETAS_HORNO, logger)};
 
+        for (Horno horno : hornos) 
+            horno.start();
         
+
         // Indexados en array para no perder acceso a cualquiera de ellos
-        Repostero[] reposteros = new Repostero[NUMERO_REPOSTEROS]; 
+        Repostero[] reposteros = new Repostero[NUMERO_REPOSTEROS];
         for (int i = 0; i < reposteros.length; i++) {
-            reposteros[i] = new Repostero("Repostero" + (i + 1), adminHornos, logger);
+            reposteros[i] = new Repostero("Repostero" + (i + 1), hornos, logger);
             reposteros[i].start();
         }
 
-        System.out.println("Todo arrancado!");
+        
+        //proc_Simulador_repostero(hornos);
     }
 }
